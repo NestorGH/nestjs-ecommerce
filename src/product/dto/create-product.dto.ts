d9d5e3c7168a5c4c7bae8d3a8bcfb91e4c4c7f45
NestorCastro <nestornica10@gmail.com>
2023-08-15 11:23:20 -0600

    added product dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsNumber, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsString()
+  @MinLength(1)
+  description: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsString()
+  @MinLength(1)
+  category: string;
+}
