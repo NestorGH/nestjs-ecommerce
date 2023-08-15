7376fb5bae600f5bc2e3bb58437c816d9ffec4b5
NestorCastro <nestornica10@gmail.com>
2023-08-15 11:15:36 -0600

    added entity and mongoose module

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,22 @@
-export class Product {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+  @Prop({
+    unique: true,
+    index: true
+  })
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  category: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product)
