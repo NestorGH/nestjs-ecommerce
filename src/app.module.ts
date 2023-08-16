32e0346cedaab725a3b9226805648ea54c6b04ef
NestorCastro <nestornica10@gmail.com>
2023-08-15 20:13:24 -0600

    added resource user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:28028/store'), ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:28028/store'), ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
