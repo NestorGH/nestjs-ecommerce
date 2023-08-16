cf72fccca4fa44a14ba128547f612d82b20a5222
NestorCastro <nestornica10@gmail.com>
2023-08-15 20:47:22 -0600

    added user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
+  async addUser(createUserDTO: CreateUserDto): Promise<User> {
+    const newUser = await this.userModel.create(createUserDTO);
+    newUser.password = await bcrypt.hash(newUser.password, 10);
+    return newUser.save();
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findUser(username: string): Promise<User | undefined> {
+    const user = await this.userModel.findOne({ username: username });
+    return user;
   }
 }
