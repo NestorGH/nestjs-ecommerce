f9b12b44635b9bd76a3582076d9e1489139c64ea
NestorCastro <nestornica10@gmail.com>
2023-08-15 21:23:49 -0600

    added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:28028/store'), ProductModule, UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:28028/store'), ProductModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
