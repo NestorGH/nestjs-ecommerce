8eaa4ea79a94da0d04b5d3a27b622c9c619e0a94
NestorCastro <nestornica10@gmail.com>
2023-08-15 20:26:08 -0600

    added user entity and create user dto

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop()
+  username: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+/*
+  @Prop()
+  roles: Role[];
+*/
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
