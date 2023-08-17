d4cc084d81728f9878728408a6768ddab7b45e4d
NestorCastro <nestornica10@gmail.com>
2023-08-17 11:59:54 -0600

    user roles management

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Role } from 'src/auth/enums/role.enum';
 
 @Schema()
 export class User extends Document {
@@ -12,10 +13,8 @@ export class User extends Document {
   @Prop()
   password: string;
 
-/*
   @Prop()
   roles: Role[];
-*/
 }
 
 export const UserSchema = SchemaFactory.createForClass(User)
